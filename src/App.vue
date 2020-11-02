ba861dbb7ae350fe740e131c6e7f0d0e27aaf5e2
k <kkraczyna@gmail.com>
2020-11-02 22:59:42 +0100

    mixins, detect mobile

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    {{isMobile}}
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
     <router-view/>
   </div>
 </template>
